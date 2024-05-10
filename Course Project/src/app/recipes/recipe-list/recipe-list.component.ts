8fa4e7982eb16d6b449752f32aa6ea52eb7e81e0
IVII1 <ivojlo2002@gmail.com>
2024-05-10 15:57:12 +0200

    module 3 update

---
+++ Course Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipes.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css',
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'recipes test',
+      'https://www.cnet.com/a/img/resize/69256d2623afcbaa911f08edc45fb2d3f6a8e172/hub/2023/02/03/afedd3ee-671d-4189-bf39-4f312248fb27/gettyimages-1042132904.jpg?auto=webp&fit=crop&height=675&width=1200',
+    ),
+
+  ];
+}
